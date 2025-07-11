b392dd42063ef8aa1376eb0abb9096ed20fcaba7
Shawon Kumar Modak <89266007+shawonkumar01@users.noreply.github.com>
2025-07-12 04:31:13 +0600

    Separating Html template Code to Different blade file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,4 +23,8 @@ class HomeController extends Controller
             return view('dashboard');
         }
     }
+    public function index()
+    {
+        return view('home.userpage');
+    }
 }
